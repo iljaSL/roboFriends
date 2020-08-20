722c103f4feef10454e4eaa68da173ae7df6030c
Ilja Smelich <ismelich@c2r6p7.hive.fi>
2020-08-20 16:23:53 +0300

    Updates

---
+++ precache-manifest.20114b0faf1c2b51dc4fadfafa06309f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4127675e9bb05c4cabd2c9c745c753ac",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "12f3a80c0f2a9e479a52",
+    "url": "/roboFriends/static/css/main.518f4a3e.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/roboFriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f3a80c0f2a9e479a52",
+    "url": "/roboFriends/static/js/main.bd0e7b58.chunk.js"
+  },
+  {
+    "revision": "b20617b5161ccb3124b5",
+    "url": "/roboFriends/static/js/runtime-main.e4e9662b.js"
+  }
+]);
